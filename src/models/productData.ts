cbf90076246f0ef09a360d12cf05ca4c1a2139f3
Bryan Rodas <brayan-rodas@hotmail.com>
2023-02-02 17:10:01 -0500

    Revert "product page"
    This reverts commit 40e2c64288f5254e326379a286e7cc7663dbae53.

--- src/models/productData.ts
+++
@@ -1,9 +0,0 @@
-export interface productData {
-  id: number;
-  name: string;
-  price: number;
-  priceWithDiscount?: number;
-  sizes?: string[];
-  images: string[];
-  description?: string;
-}
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,43 +1,7 @@
-import { Link } from "react-router-dom";
-import { Carousel } from "../../components/Carousel";
-import { produtsList, alsoLike } from "../data/products";
-
 export interface ProductsInterface {}
 
 const Products: React.FC<ProductsInterface> = () => {
-  const product = produtsList[0];
-  const sizes = product.sizes;
-  return (
-    <div>
-      {/* <Carousel autoPlay={false} showButtons={true} /> */}
-      <img src={`/images/${product.images[0]}`} />
-      <div>
-        <span>{product.name}</span>
-      </div>
-      <img src={`/images/stars.svg`} />
-      <div>
-        <span>{product.price}</span>
-        <span>{product.priceWithDiscount}</span>
-      </div>
-      <div>
-        {sizes?.map((size, index) => (
-          <span key={index}>{size}</span>
-        ))}
-      </div>
-      <div>
-        <span>counter</span>
-        <button>Agregar al carrito</button>
-      </div>
-      <div>
-        <span>DESCRIPCIÓN:</span>
-        <span>{product.description}</span>
-        <Link to="">Leer más</Link>
-      </div>
-      <div>
-        <span>TAMBIÉN TE PODRÍA GUSTAR</span>
-      </div>
-    </div>
-  );
+  return <div>Products</div>;
 };
 
 export default Products;
