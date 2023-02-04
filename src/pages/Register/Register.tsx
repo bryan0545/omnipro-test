56bd9cf0e3b62b9091074e35cb46773e163b75ed
Bryan Rodas <brayan-rodas@hotmail.com>
2023-02-04 14:16:45 -0500

    Register custom hook

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,102 +1,12 @@
-import { useState } from "react";
-import { TextInput } from "../../components";
+import { TextInput, CustomButton, SuccessPage } from "../../components";
 import { PublicLayout } from "../../layouts/PublicLayout";
-import CustomButton from "../../components/CustomButton/CustomButton";
-import { SuccessPage } from "../../components/SuccessPage";
-import { useNavigate } from "react-router-dom";
-import { PublicRoutes, userInfo } from "../../models";
-import { createUser } from "../../services/auth";
-import { saveLocalStorageObj } from "../../utilities";
-import { localstorageKeys } from "../../constants/constants";
+import { useRegister } from "../../hooks/useRegister";
 import "./register.scss";
 
 export interface RegisterInterface {}
 
 const Register: React.FC<RegisterInterface> = () => {
-  const navigation = useNavigate();
-  const [success, setSuccess] = useState(false);
-  const [formError, setFormError] = useState("");
-  const [loadingRequest, setLoadingRequest] = useState(false);
-  const [registerForm, setRegisterForm] = useState({
-    name: "",
-    lastname: "",
-    email: "",
-    password: "",
-  });
-
-  const inputs = [
-    {
-      id: 1,
-      type: "text",
-      name: "name",
-      placeholder: "Nombre*",
-    },
-    {
-      id: 2,
-      type: "text",
-      name: "lastname",
-      placeholder: "Apellido*",
-    },
-    {
-      id: 3,
-      type: "email",
-      name: "email",
-      placeholder: "E-mail*",
-    },
-    {
-      id: 4,
-      type: "password",
-      name: "password",
-      placeholder: "Contraseña*",
-    },
-  ];
-
-  const validateForm = () => {
-    for (const prop in registerForm) {
-      const key = prop as keyof typeof registerForm;
-      if (registerForm[key] === "") {
-        setFormError("Por favor, diligencia los campos marcados");
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const registerUser = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!validateForm()) return;
-    setLoadingRequest(true);
-    try {
-      const res = await (await createUser()).json();
-      if (res.status !== 201) {
-        setLoadingRequest(false);
-        return setFormError("Algo salió mal, intentalo mas tarde");
-      }
-
-      const userData: userInfo = {
-        name: registerForm.name,
-        password: registerForm.password,
-      };
-      saveLocalStorageObj(localstorageKeys.registeredUser, userData);
-      setLoadingRequest(false);
-
-      setSuccess(true);
-      setTimeout(() => {
-        navigation(PublicRoutes.LOGIN);
-      }, 2000);
-    } catch {
-      setLoadingRequest(false);
-      setFormError("Algo salió mal, intentalo mas tarde");
-    }
-  };
-
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRegisterForm({
-      ...registerForm,
-      [e.target.name]: e.target.value,
-    });
-  };
-
+  const { registerUser, inputs, registerForm, onChangeHandler, formError, loadingRequest, success } = useRegister();
   return (
     <>
       <PublicLayout titleForm="Completa tus datos para registrarte" backgroundImageUrl={"images/registerBackground.png"}>
